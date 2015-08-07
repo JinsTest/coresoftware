2dcac412dd2bab76e39bb5f8e3e8f2e62be3bcf0
Nils Feege <nils.feege@stonybrook.edu>
2015-08-06 23:33:22 -0400

    small update Doxygen documentation

--- simulation/g4simulation/g4cemc/CaloTowerID.h
+++ simulation/g4simulation/g4cemc/CaloTowerID.h
@@ -1,6 +1,6 @@
 #include <string>
 
-/* Namespace with functions to set / extract
+/*! Namespace with functions to set / extract
  * calorimeter ID , which is stored as highest 8 bits of the CaloTowerID integer.
  * This 32 bit integer uniquely identifies each tower in the whole detector- the highest eight bits
  * contain an ID for the calorimeter, and the lower 24 bits contain an ID for the tower within
@@ -9,7 +9,10 @@
 namespace CaloTowerID
 {
 
-  /* List of available calorimeter IDs
+  /*! List of available calorimeter IDs.
+   * This enum can be safely extended up to 254 entries total but NO ALTERATIONS of existing entries.
+   * '0' is effectively reserved for 'no calorimeter defined' which should not happen when you run the code-
+   * each tower is to be in SOME calorimeter.
    */
   enum Calorimeter_t {
     cECAL=1,
@@ -21,13 +24,13 @@ namespace CaloTowerID
   };
 
 
-  /* binary 00000000111111111111111111111111 to set
+  /*! binary 00000000111111111111111111111111 to set
    * calorimeter ID to 0
    */
   int caloID_zero = 16777215;
 
 
-  /* Returns the local tower ID, i.e. it strips the highest 8 bits
+  /*! Returns the local tower ID, i.e. it strips the highest 8 bits
    * which encode the calorimeter this tower is in
    */
   int GetLocalCaloTowerID( int caloTowerID ){
@@ -37,7 +40,7 @@ namespace CaloTowerID
   }
 
 
-  /* Returns new CaloTowerID with the highest 8 bits set to encode
+  /*! Returns new CaloTowerID with the highest 8 bits set to encode
    * given calorimeter
    */
   int SetCalorimeter( int caloTowerID , Calorimeter_t caloID ){
@@ -56,7 +59,7 @@ namespace CaloTowerID
   }
 
 
-  /* Extract ID number of calorimeter from CaloTowerID
+  /*! Extract ID number of calorimeter from CaloTowerID
    */
   int GetCalorimeter( int caloTowerID ){
 
@@ -66,7 +69,7 @@ namespace CaloTowerID
   }
 
 
-  /* Extract name of calorimeter from CaloTowerID
+  /*! Extract name of calorimeter from CaloTowerID
    */
   std::string GetCalorimeterName( int caloTowerID ){
 
