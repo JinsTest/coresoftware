51fecd2cdf850080acd72e3c678fe5857f7a7853
Nils Feege <nils.feege@stonybrook.edu>
2015-08-07 00:26:57 -0400

    Make CaloTowerID unsigned int instead of signed int

--- simulation/g4simulation/g4cemc/CaloTower.h
+++ simulation/g4simulation/g4cemc/CaloTower.h
@@ -16,11 +16,9 @@ class CaloTower : public PHObject {
   virtual int isValid() const { PHOOL_VIRTUAL_WARN("isValid()"); return 0; }
   virtual void identify(std::ostream& os=std::cout) const { PHOOL_VIRTUAL_WARN("identify()"); }
 
-  virtual int get_bineta() const { PHOOL_VIRTUAL_WARN("get_ieta()"); return -1; }
-  virtual int get_binphi() const { PHOOL_VIRTUAL_WARN("get_iphi()");; return -1; }
-  virtual float get_energy() const { PHOOL_VIRTUAL_WARN("get_energy()"); return 0.0; }
+  virtual unsigned int get_id() const { PHOOL_VIRTUAL_WARN("get_id()"); return 0; }
 
-  virtual bool is_adjacent(CaloTower& tower) { PHOOL_VIRTUAL_WARNING; return false; }
+  virtual float get_energy() const { PHOOL_VIRTUAL_WARN("get_energy()"); return 0.0; }
 
   virtual std::pair< std::map<unsigned int,float>::const_iterator, std::map<unsigned int,float>::const_iterator > get_g4cells();
   virtual void add_ecell(const unsigned int g4cellid, const float ecell) {PHOOL_VIRTUAL_WARN("add_ecell(const unsigned int g4cellid, const float ecell)"); return;}
